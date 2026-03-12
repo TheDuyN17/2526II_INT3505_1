350c3e721f9ec430934fa3017811d4da88a2e01d
TheDuyN17 <22024578@vnu.edu.vn>
2026-03-12 17:37:12 +0700

    v1: minimal Flask service - GET/POST /users with dict storage

--- w2/codew2.py
+++ w2/codew2.py
@@ -2,85 +2,32 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
-# Model User
-class User:
-    def __init__(self, uid, name, email):
-        self.uid = uid
-        self.name = name
-        self.email = email
-
-    def to_json(self):
-        return {
-            "id": self.uid,
-            "name": self.name,
-            "email": self.email
-        }
-
-
-# Fake database
+# Fake database - danh sách dict đơn giản
 users = [
-    User("1", "Nguyen The Duy", "theduynguyen27@gmail.com"),
+    {"id": "1", "name": "Nguyen The Duy", "email": "theduynguyen27@gmail.com"}
 ]
 
-
-# Helper: tìm user theo id
-def find_user(uid):
-    for i in range(len(users)):
-        if users[i].uid == uid:
-            return users[i], i
-    return None, None
+next_id = 2
 
 
-# GET + POST /users
-@app.route("/users", methods=["GET", "POST"])
-def handle_users():
+@app.route("/users", methods=["GET"])
+def get_users():
+    return jsonify(users), 200
 
-    if request.method == "GET":
-        result = list(map(lambda u: u.to_json(), users))
-        return jsonify(result), 200
 
+@app.route("/users", methods=["POST"])
+def create_user():
+    global next_id
     data = request.get_json()
-
-    if data is None:
-        return jsonify({"error": "Invalid data"}), 400
-
-    user = User(data["id"], data["name"], data["email"])
+    user = {
+        "id": str(next_id),
+        "name": data["name"],
+        "email": data["email"]
+    }
+    next_id += 1
     users.append(user)
-
-    return jsonify(user.to_json()), 201
-
-
-# PUT PATCH DELETE /users/<id>
-@app.route("/users/<uid>", methods=["PUT", "PATCH", "DELETE"])
-def handle_user_by_id(uid):
-
-    user, index = find_user(uid)
-
-    if user is None:
-        return "", 404
-
-    if request.method == "DELETE":
-        users.pop(index)
-        return "", 204
-
-    data = request.get_json()
-
-    if data is None:
-        return "", 400
-
-    if request.method == "PUT":
-        updated = User(uid, data["name"], data["email"])
-        users[index] = updated
-        return jsonify(updated.to_json()), 200
-
-    if request.method == "PATCH":
-        user.name = data.get("name", user.name)
-        user.email = data.get("email", user.email)
-        return jsonify(user.to_json()), 200
+    return jsonify(user), 201
 
 
 if __name__ == "__main__":
-    PORT = 5000
-    print(f"Server running at http://localhost:{PORT}")
-    app.run(port=PORT)
+    app.run(port=5000)
